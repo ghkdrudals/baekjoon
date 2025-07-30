cfa6f1bf15213f5d1284f3b75d8087693e8d5e7a
황경민 <aa01046044029@gmail.com>
2025-07-30 21:28:05 +0900

    [Bronze II] Title: 분산처리, Time: 56 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1009. 분산처리/분산처리.py
+++ 백준/Bronze/1009. 분산처리/분산처리.py
@@ -1,16 +1,18 @@
-t = int(input())
-for _ in range(t):
+n = int(input())
+for _ in range(n):
     a, b = map(int, input().split())
-    a %= 10  # 마지막 자리만 보면 되니까!
-    
+    a %= 10  # 마지막 자리만 보면 돼!
+
     if a == 0:
         print(10)
-    else:
-        pattern = []
-        tmp = a
-        while tmp not in pattern:
-            pattern.append(tmp)
-            tmp = (tmp * a) % 10
-        # 주기적으로 반복되기 때문에 b를 주기의 길이로 나눠서 인덱스 결정
-        idx = (b - 1) % len(pattern)
-        print(pattern[idx])
+        continue
+
+    # 패턴 저장
+    pattern = []
+    tmp = a
+    while tmp not in pattern:
+        pattern.append(tmp)
+        tmp = (tmp * a) % 10
+
+    idx = (b - 1) % len(pattern)
+    print(pattern[idx])
